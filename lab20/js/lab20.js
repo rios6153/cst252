6e3896d00696835a8423552b0c42cf25f4855f1c
Arturo <arrios@csumb.edu>
2022-04-26 21:46:40 -0700

    lab 20

---
+++ lab20/js/lab20.js
@@ -0,0 +1,50 @@
+/*
+* Authors: Suzy Ford & Art Rios
+* Created: 26 April 2022
+* License: Public Domain
+*/
+// :)
+
+var URL = "https://xkcd.com/info.0.json";
+var URLpre = "https://xkcd.com/";
+var URLpost = "info.0.json";
+
+
+function getComic(num) {
+  if (typeof num === 'undefined') {
+    numStr = "";
+  } else {
+    numStr = num.toString() + "/";
+  }
+  var ourURL = URLpre + numStr + URLpost;
+  console.log("Our new URL:", ourURL);
+
+  $.ajax({
+
+      url: ourURL,
+
+      type: "GET",
+  })
+
+  .done(function(data) {
+
+      var imageUrl = data.img;
+
+      var title = data.title;
+      console.log("title:", title);
+
+      var alt = data.alt;
+      console.log("alt:", alt);
+
+      $("#output").imageUrl(imageUrl);
+      $("#output").title(title);
+      $("#output").append(alt);
+  })
+  .fail(function(){
+    console.log("^^ Sorry, don't mind this error, everything is going to be okay!");
+    console.log("Keep going, enjoy the rest of your day! :-)");
+  })
+
+}
+
+getComic();
